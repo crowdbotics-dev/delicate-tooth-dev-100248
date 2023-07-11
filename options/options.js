d68bb4d271d14211e29f61fcc243ad0e6f3b7a7e
Crowdbotics <engineering@crowdbotics.com>
2023-07-11 23:15:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-tooth-dev-100248",
+  url: "https://delicate-tooth-dev-100248.botics.co",
+  api: "https://delicate-tooth-dev-100248.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
